73194b6a55680e0941b32d196520764e84538a0e
Sean-Wang <exiang.bee@gmail.com>
2012-10-22 22:48:05 +0800

    Initial Commit

---
+++ OPLaunchAd/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  OPLaunchAd
+//
+//  Created by Venus on 12-10-22.
+//  Copyright (c) 2012年 opomelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ OPLaunchAd/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OPLaunchAd
+//
+//  Created by Venus on 12-10-22.
+//  Copyright (c) 2012年 opomelo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
